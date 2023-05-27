dd8cecbca1cea1bea393530a328904b184138ccf
22P35A0384 <134734254+22P35A0384@users.noreply.github.com>
2023-05-27 10:07:21 +0530

    n = int(input()) for i in range(n):     for l in range(n-i-1):         print(" ",end="")     for k in range(i,0,-1):         print(k,end="")     for j in range(0,i+1):         print(j,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,6 +1,9 @@
 n = int(input())
-l = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 for i in range(n):
-    for j in range(n-i):
-        print(l[n-i-1],end=" ")
+    for l in range(n-i-1):
+        print(" ",end="")
+    for k in range(i,0,-1):
+        print(k,end="")
+    for j in range(0,i+1):
+        print(j,end="")
     print()
