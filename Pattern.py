eb6c8b43a3a9d2db9619c03861a492d6041728a9
22P35A0384 <134734254+22P35A0384@users.noreply.github.com>
2023-05-27 10:33:09 +0530

    n = int(input()) for i in range(1,n+1):     for k in range(n-i):         print(' ',end='')     for j in range(1,i+1):         print(j,end="")     for a in range(1,i):         print(i-a,end="")     print()

---
+++ Pattern.py
@@ -0,0 +1,9 @@
+n = int(input())
+for i in range(1,n+1):
+    for k in range(n-i):
+        print(' ',end='')
+    for j in range(1,i+1):
+        print(j,end="")
+    for a in range(1,i):
+        print(i-a,end="")
+    print()
